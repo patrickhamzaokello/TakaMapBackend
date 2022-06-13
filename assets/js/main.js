76c70877360310a848b022604e375332b5f0dd0e
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-06-13 19:50:51 +0300

    update on type

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,9 @@ async function getInfrastructure() {
         let template = `
             
             <div class="infras_card" style="border-radius: 20px;background-color:#228765 ; padding: 2em; color: #fff; font-size: 15px;">
-            <h1 style="color: #F6FFEE; font-size: 22px;"></h1>
-            <p style="margin: 0;color: #CDEDCB;">Pece Prison Primary School</p>
-            <p style="margin: 0;color:#36CC7C ;margin-top: 1em;"><span style="color: #CDEDCB;">Aim</span> school</p>
+            <h1 style="color: #F6FFEE; font-size: 22px;">${type}</h1>
+            <p style="margin: 0;color: #CDEDCB;">${description}</p>
+            <p style="margin: 0;color:#36CC7C ;margin-top: 1em;"><span style="color: #CDEDCB;">Aim</span>${aim}</p>
             <p style="margin: 0;"><span style="color: #CDEDCB;">Contact</span> 0787250196</p>
             <p style="margin: 0;"><span style="color: #CDEDCB;">Install Date</span> 8th Jun 2022</p>
             <p style=" margin: 0; color: #D9D055; margin-top: 1em; font-size: 12px;">This location may change based on the tracker device</p>
