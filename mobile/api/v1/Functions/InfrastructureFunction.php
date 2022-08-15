40488402c039915b6633e4d64404c71b99a62ed0
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-08-15 15:50:56 +0300

    near me working

--- mobile/api/v1/Functions/InfrastructureFunction.php
+++ mobile/api/v1/Functions/InfrastructureFunction.php
@@ -1,4 +1,5 @@
 <?php
+
 class InfrastructureFunction
 {
 
@@ -8,6 +9,7 @@ class InfrastructureFunction
     public $typeid;
     private $conn;
     public $page;
+    public $distance;
 
     public function __construct($con)
     {
@@ -33,7 +35,6 @@ class InfrastructureFunction
             $total_pages = ceil($total_rows / $no_of_records_per_page);
 
 
-
             $itemRecords["page"] = $this->page;
             $itemRecords["infrastructure"] = array();
             $itemRecords["types"] = array();
@@ -45,17 +46,16 @@ class InfrastructureFunction
             $stmt->execute();
             $stmt->bind_result($this->id);
 
-            $ins_id =array();
+            $ins_id = array();
 
-            while ($stmt->fetch()){
+            while ($stmt->fetch()) {
                 array_push($ins_id, $this->id);
             }
-            foreach ($ins_id as $id){
+            foreach ($ins_id as $id) {
                 $temp = array();
 
 
-
-                $type = new Infrastructure($this->conn,$id);
+                $type = new Infrastructure($this->conn, $id);
 
                 $temp['id'] = $type->getId();
                 $temp['aim'] = $type->getAim();
@@ -72,15 +72,93 @@ class InfrastructureFunction
             $stmt->execute();
             $stmt->bind_result($this->typeid);
 
-            $ins_types_id =array();
+            $ins_types_id = array();
 
-            while ($stmt->fetch()){
+            while ($stmt->fetch()) {
                 array_push($ins_types_id, $this->typeid);
             }
-            foreach ($ins_types_id as $id){
+            foreach ($ins_types_id as $id) {
                 $temp = array();
 
-                $type = new InfrastructureTypes($this->conn,$id);
+                $type = new InfrastructureTypes($this->conn, $id);
+
+                $temp['id'] = $type->getId();
+                $temp['name'] = $type->getName();
+                $temp['iconpath'] = $type->getIconPath();
+//
+                array_push($itemRecords["types"], $temp);
+            }
+
+            return $itemRecords;
+        } else {
+            return "No Record";
+        }
+    }
+
+    function Infrastructures_Near_Me()
+    {
+        $itemRecords = array();
+
+        $this->page = htmlspecialchars(strip_tags($_GET["page"]));
+
+        if ($this->page) {
+            $this->page = floatval($this->page);
+            $no_of_records_per_page = 100;
+            $offset = ($this->page - 1) * $no_of_records_per_page;
+
+            $sql = "SELECT COUNT(*) as count FROM " . $this->itemsTable . " limit 1";
+            $result = mysqli_query($this->conn, $sql);
+            $data = mysqli_fetch_assoc($result);
+            $total_rows = floatval($data['count']);
+            $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+            $itemRecords["page"] = $this->page;
+            $itemRecords["infrastructure"] = array();
+            $itemRecords["types"] = array();
+            $itemRecords["total_pages"] = $total_pages;
+            $itemRecords["total_results"] = $total_rows;
+
+
+            $stmt = $this->conn->prepare("SELECT `id`, ROUND((6371 * acos( cos(radians(2.7847098484273696)) * cos(radians(latitude)) * cos(radians(longitude) - radians(32.3069465264721)) + sin(radians(2.7847098484273696)) * sin(radians(latitude)))), (2)) AS distance FROM " . $this->itemsTable . " HAVING distance < 50 ORDER BY distance Limit $offset, $no_of_records_per_page;");
+            $stmt->execute();
+            $stmt->bind_result($this->id , $this->distance);
+
+            $ins_id = array();
+
+            while ($stmt->fetch()) {
+                array_push($ins_id, $this->id);
+            }
+            foreach ($ins_id as $id) {
+                $temp = array();
+
+
+                $type = new Infrastructure($this->conn, $id);
+
+                $temp['id'] = $type->getId();
+                $temp['aim'] = $type->getAim();
+                $temp['description'] = $type->getDescription();
+                $temp['longitude'] = $type->getLongitude();
+                $temp['latitude'] = $type->getLatitude();
+                $temp['type'] = $type->getType();
+                $temp['iconpath'] = $type->getIconPath();
+//
+                array_push($itemRecords["infrastructure"], $temp);
+            }
+
+            $stmt = $this->conn->prepare("SELECT `id` FROM $this->infrastructuretypesTable ORDER BY `infrastructuretypes`.`name` ASC");
+            $stmt->execute();
+            $stmt->bind_result($this->typeid);
+
+            $ins_types_id = array();
+
+            while ($stmt->fetch()) {
+                array_push($ins_types_id, $this->typeid);
+            }
+            foreach ($ins_types_id as $id) {
+                $temp = array();
+
+                $type = new InfrastructureTypes($this->conn, $id);
 
                 $temp['id'] = $type->getId();
                 $temp['name'] = $type->getName();
---
+++ mobile/api/v1/Requests/near_me.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../../../../admin/config.php';
+include_once '../Classes/InfrastructureTypes.php';
+include_once '../Classes/Infrastructure.php';
+include_once '../Functions/InfrastructureFunction.php';
+
+$database = new Database();
+$db = $database->getConnString();
+ 
+$infrastructure = new InfrastructureFunction($db);
+
+$infrastructure->page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '0';
+
+$result = $infrastructure->Infrastructures_Near_Me();
+
+if($result){    
+    http_response_code(200);     
+    echo json_encode($result);
+}else{     
+    http_response_code(404);     
+    echo json_encode(
+        array("message" => "No item found.")
+    );
+} 
+?>
